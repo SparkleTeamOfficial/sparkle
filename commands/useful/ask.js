1b4f5911d3cbf19a6eb924c3e27b1a197f3a7b52
SparkleTeamOfficial <50514290+SparkleTeamOfficial@users.noreply.github.com>
2019-05-11 20:00:29 +0200

    Update ask.js

--- commands/useful/ask.js
+++ commands/useful/ask.js
@@ -9,7 +9,7 @@ return;
         let url = `http://api.wolframalpha.com/v1/conversation.jsp?appid=PKEX2L-X3HXQHTEJQ&i=${args.join(" ")}`
         get(url).then(res => {
 if(res.body.error){
-return message.reply("I'm not as smart as the Alpha :(")
+return message.reply("STFU I'M NOT SMART REEEEE")
 }
             const embed = new Discord.RichEmbed()
             .setColor("fea5ff")
